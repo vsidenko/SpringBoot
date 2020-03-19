95792f646dc69e3a40104d7d99e7ff1f788efc6d
vsidenko <vsidenko89@gmail.com>
2020-03-19 13:03:01 +0200

    initial commit

---
+++ src/main/java/com/learnspring/dobro/DobroApplication.java
@@ -0,0 +1,13 @@
+package com.learnspring.dobro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DobroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learnspring/dobro/DobroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.dobro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DobroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
